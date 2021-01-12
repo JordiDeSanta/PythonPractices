eca1e783aee0f50c07dd86be43ce8a7b1bc88ee3
Jordi <jordi.bravo.bad@gmail.com>
2021-01-12 17:13:37 -0300

    Username creator finished

--- usernamecreator.py
+++ usernamecreator.py
@@ -1,5 +1,6 @@
 print("Create a username: ")
 
+minimunlength = 6
 aprobated = False
 
 # While the password is not aprobated, ask to the user again
@@ -7,7 +8,7 @@ while aprobated == False:
     # USername ask
     username = input()
     # Check the number of characters of the introduced username
-    if(len(username) < 6):
+    if(len(username) < minimunlength):
         print("Please insert an user name with 6 characters minimun")
     else:
         print("Success")
